628d13e8460b2ce6cf323a7fb984b1930f92fcc7
Shreyas Gandhi <100945644+ShreyasGandhi0607@users.noreply.github.com>
2023-03-18 21:33:08 +0530

    Add files via upload

---
+++ JAVA/Program1/src/com/shreyas/Basics.java
@@ -0,0 +1,20 @@
+package com.shreyas;
+
+public class Basics {
+    public static void main(String[] args) {
+//        int a = 10;
+//        if(a == 10){
+//            System.out.println("Hello ");
+//        }
+//        int i  = 1;
+//        while(count!=5){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        for(int i=0 ; i<6;i++){
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ JAVA/Program1/src/com/shreyas/Inputs.java
@@ -0,0 +1,16 @@
+package com.shreyas;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter roll number : ");
+        int roll_no = input.nextInt();
+        System.out.println("The roll number is "+ roll_no);
+        int a = 234_000_000;
+        System.out.println(a);
+        float marks = input.nextFloat();
+        System.out.println(marks);
+    }
+}
